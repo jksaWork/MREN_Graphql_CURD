9af11ccd2ed641f3bbf91256b11beeef471e9013
jksa.work.1@gmail.com <jksa.work.1@gmail.com>
2023-07-18 12:18:56 -0700

    Test Queries

--- client/tests/Header.test.tsx
+++ client/tests/Header.test.tsx
@@ -1,12 +1,13 @@
-import { render, screen } from "@testing-library/react";
+import { logRoles, render, screen } from "@testing-library/react";
 // import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
 import Header from "../src/components/Header";
 
 test("Header - 1 Rednered Sueecc", async () => {
   // ARRANGE
-  render(<Header />);
+  const { container } = render(<Header />);
   // ACT
+  // logRoles(container);
   // await userEvent.click(screen.getByText("Load Greeting"));
   const el = await screen.getByText("Projects Mangement");
   // ASSERT !
